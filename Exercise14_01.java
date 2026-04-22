fb158589a27cd5d059031aedc2920c73fa6ec805
oyekinni <165745004+oyekinni@users.noreply.github.com>
2026-04-22 16:28:25 -0400

    Exercise14_01.java

--- Exercise14_01.java
+++ Exercise14_01.java
@@ -1,17 +1,45 @@
-import javafx.application.Application; import javafx.geometry;Insets; javafx.scene;Scene; import javafx.scene.image.Image; import javafx.scene.image.ImageView; import javafx.scene.layout.GridPane; import javafx.stage.Stage;
+import javafx.application.Application; 
+import javafx.geometry.Insets; 
+import javafx.scene.Scene; 
+import javafx.scene.image.Image; 
+import javafx.scene.image.ImageView; 
+import javafx.scene.layout.GridPane; 
+import javafx.stage.Stage;
 
-public class Exercise14_01 is an Application class that
+public class Exercise14_01 extends Application {
 
-    @Overide public void start(Stage primaryStage) { GridPane pane = new GridPane(); pane.setPadding(new Insets(10)); pane.setHgap(10); pane.setVgap(10);
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane pane = new GridPane(); 
+        pane.setPadding(new Insets(10)); 
+        pane.setHgap(10); 
+        pane.setVgap(10);
 
-        ImageView germany = new ImageView(new Image("file:images/germany.png")); ImageView china = new ImageView(new Image("file:images/china.png")); ImageView france = new ImageView(new Image("file:images/france.png")); ImageView us = new ImageView(new Image("file:images/us.png"));
+        ImageView germany = new ImageView(new Image("file:images/germany.png")); 
+        ImageView china = new ImageView(new Image("file:images/china.png")); 
+        ImageView france = new ImageView(new Image("file:images/france.png")); 
+        ImageView us = new ImageView(new Image("file:images/us.png"));
 
-        ImageView[] pictures = {germany, china, france, and us};
+        ImageView[] pictures = {germany, china, france, us};
 
-        for (ImageView imageView : images) { imageView.setFitWidth(150); imageView.setFitHeight(100); imageView.setPreserveRatio(true); }
+        for (ImageView img : pictures) { 
+            img.setFitWidth(150); 
+            img.setFitHeight(100); 
+            img.setPreserveRatio(true); 
+        }
 
-        pane.add(germany, 0, 0); pane.add(china, 1, 0); pane.add(france, 0, 1); and pane.add(us, 1, 1);
+        pane.add(germany, 0, 0); 
+        pane.add(china, 1, 0); 
+        pane.add(france, 0, 1); 
+        pane.add(us, 1, 1);
 
-        Scene scene = new Scene(pane); primaryStage.setTitle("Exercise14_01"); primaryStage.setScene(scene); primaryStage.show();
+        Scene scene = new Scene(pane); 
+        primaryStage.setTitle("Exercise14_01"); 
+        primaryStage.setScene(scene); 
+        primaryStage.show();
+    }
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) { 
+        launch(args); 
+    }
+}
